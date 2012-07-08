39a6ae9e5359e32eea174b73fc526d4da14a195d
jimmie33 <jimmie33@gmail.com>
2012-07-07 23:26:20 -0400

    Add paper link; Add CMakeLists.txt.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 *	Implemetation of the multi-person tracking system described in paper
 *	"Online Multi-person Tracking by Tracker Hierarchy", Jianming Zhang, 
 *	Liliana Lo Presti, Stan Sclaroff, AVSS 2012
+*	http://www.cs.bu.edu/groups/ivc/html/paper_view.php?id=268
 *
 *	Copyright (C) 2012 Jianming Zhang
 *
